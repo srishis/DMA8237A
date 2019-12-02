5301ce8b526a374077800ab140bb0c71ae25b907
Rishitosh Sawant <srishis.214@gmail.com>
2019-12-02 00:41:06 -0800

    Added DMA register interface modports

--- dmaRegIf.sv
+++ dmaRegIf.sv
@@ -16,10 +16,10 @@ logic [15:0] baseWordReg[0];
 logic [15:0] baseWordReg[1];
 logic [15:0] baseWordReg[2];
 logic [15:0] baseWordReg[3];
-logic [7:0]  modeReg[0];
-logic [7:0]  modeReg[1];
-logic [7:0]  modeReg[2];
-logic [7:0]  modeReg[3];
+logic [5:0]  modeReg[0];
+logic [5:0]  modeReg[1];
+logic [5:0]  modeReg[2];
+logic [5:0]  modeReg[3];
 logic [7:0]  commandReg;
 logic [7:0]  requestReg;
 logic [7:0]  maskReg;
@@ -39,31 +39,31 @@ modport FSM(
 	);
 
 modport REG(
-		output [15:0] currAddrReg[0];
-		output [15:0] currAddrReg[1];
-		output [15:0] currAddrReg[2];
-		output [15:0] currAddrReg[3];
-		output [15:0] currWordReg[0];
-		output [15:0] currWordReg[1];
-		output [15:0] currWordReg[2];
-		output [15:0] currWordReg[3];
-		output [15:0] baseAddrReg[0];
-		output [15:0] baseAddrReg[1];
-		output [15:0] baseAddrReg[2];
-		output [15:0] baseAddrReg[3];
-		output [15:0] baseWordReg[0];
-		output [15:0] baseWordReg[1];
-		output [15:0] baseWordReg[2];
-		output [15:0] baseWordReg[3];
-		output [5:0]  modeReg[0];
-		output [5:0]  modeReg[1];
-		output [5:0]  modeReg[2];
-		output [5:0]  modeReg[3];
-		output [7:0]  commandReg;
-		output [7:0]  requestReg;
-		output [7:0]  maskReg;
-		output [7:0]  tempReg;
-		output [7:0]  statusReg;	
+		output [15:0] currAddrReg[0],
+		output [15:0] currAddrReg[1],
+		output [15:0] currAddrReg[2],
+		output [15:0] currAddrReg[3],
+		output [15:0] currWordReg[0],
+		output [15:0] currWordReg[1],
+		output [15:0] currWordReg[2],
+		output [15:0] currWordReg[3],
+		output [15:0] baseAddrReg[0],
+		output [15:0] baseAddrReg[1],
+		output [15:0] baseAddrReg[2],
+		output [15:0] baseAddrReg[3],
+		output [15:0] baseWordReg[0],
+		output [15:0] baseWordReg[1],
+		output [15:0] baseWordReg[2],
+		output [15:0] baseWordReg[3],
+		output [5:0]  modeReg[0],
+		output [5:0]  modeReg[1],
+		output [5:0]  modeReg[2],
+		output [5:0]  modeReg[3],
+		output [7:0]  commandReg,
+		output [7:0]  requestReg,
+		output [7:0]  maskReg,
+		output [7:0]  tempReg,
+		output [7:0]  statusReg	
 	);
 
 endinterface
