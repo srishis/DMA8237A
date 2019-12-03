2aa29428c7605bf785c05c91bfc18e2c722fe941
Rishitosh Sawant <srishis.214@gmail.com>
2019-12-02 19:12:38 -0800

    name changes related to interfaces.

--- DmaDatapath.sv
+++ DmaDatapath.sv
@@ -1,16 +1,15 @@
-`include "DmaPackage.svh"
-module dmaDatapath(dma_if.DUT dif, dmaControlIf cif);
+
+module DmaDatapath(dma_if.DUT dif, DmaControlIf cif, DmaDatapathIf dpf);
 
 import DmaPackage::*;
-
 
 // Data bus logic
 
 // Databus DB as input 
-always_ff@(posedge dma_if.CLK) if(IDLE_CYCLE && !dif.IOW_N) cif.ioDataBuf <= dif.DB;  // @Srini: CPU send register programming during IO Write in IDLE_CYCLE 
+always_ff@(posedge dma_if.CLK) if(IDLE_CYCLE && !dif.IOW_N) dpf.ioDataBuf <= dif.DB;  // @Srini: CPU send register programming during IO Write in IDLE_CYCLE 
 
 // Databus DB as output 
-always_comb dif.DB = (IDLE_CYCLE && ~dif.IOR_N) ? cif.ioDataBuf : 8'bz;  // @Srini: CPU reads data from registers so write data from registers to be driven on DB lines in ioDataBuf
+always_comb dif.DB = (IDLE_CYCLE && ~dif.IOR_N) ? dpf.ioDataBuf : 8'bz;  // @Srini: CPU reads data from registers so write data from registers to be driven on DB lines in ioDataBuf
 
 // IO Read logic
 
@@ -41,9 +40,9 @@ always_comb dif.ADSTB <= cif.adstb;  // when we make ADSTB = 1, MSB address from
 
 // Address Bus logic
 
-always_ff@(posedge dma_if.CLK) if(IDLE_CYCLE) cif.ioAddrBuf <= dif.ADDR_L;  // @Srini: ioAddrBuf holds the read address of the register to be accessed by CPU in program condition
-always_comb dif.ADDR_U = (ACTIVE_CYCLE) ? cif.outAddrBuf : 4'bz;  // @Srini: write to outAddrBuf upper 4 bits of output address from TEMP ADDRESS REGISTER and this register gets the address from dif.DB lines 
-always_comb dif.ADDR_L = (ACTIVE_CYCLE) ? cif.ioAddrBuf : 4'bz;   //  @Srini: write to ioAddrBuf lower 4 bits of address output from TEMP ADDRESS REGISTER and this register gets the address from dif.DB lines
+always_ff@(posedge dma_if.CLK) if(IDLE_CYCLE) dpf.ioAddrBuf <= dif.ADDR_L;  // @Srini: ioAddrBuf holds the read address of the register to be accessed by CPU in program condition
+always_comb dif.ADDR_U = (ACTIVE_CYCLE) ? dpf.outAddrBuf : 4'bz;  // @Srini: write to outAddrBuf upper 4 bits of output address from TEMP ADDRESS REGISTER and this register gets the address from dif.DB lines 
+always_comb dif.ADDR_L = (ACTIVE_CYCLE) ? dpf.ioAddrBuf : 4'bz;   //  @Srini: write to ioAddrBuf lower 4 bits of address output from TEMP ADDRESS REGISTER and this register gets the address from dif.DB lines
 
 
 
