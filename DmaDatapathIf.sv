8b12234d8863b23328443856cdb227b5b65d9257
Rishitosh Sawant <srishis.214@gmail.com>
2019-12-02 19:14:44 -0800

    name changed for interface

--- DmaDatapathIf.sv
+++ DmaDatapathIf.sv
@@ -1,4 +1,9 @@
-interface dmaRegIf(input logic CLK ,RESET);
+interface DmaDatapathIf(input logic CLK ,RESET);
+
+// Datapath Buffers
+logic [7:0] ioDataBuf;
+logic [3:0] ioAddrBuf;
+logic [3:0] outAddrBuf;
 
 logic [15:0] currAddrReg[4];
 logic [15:0] currWordReg[4];
@@ -8,17 +13,8 @@ logic [5:0]  modeReg[4];
 logic [7:0]  commandReg;
 logic [7:0]  requestReg;
 logic [7:0]  maskReg;
-logic [7:0]  tempAddrReg;
-logic [7:0]  tempWordReg;
 logic [7:0]  tempReg;
 logic [7:0]  statusReg;
-	
-// reg signals
-logic [7:0] readBuf;
-logic [7:0] writeBuf ;
-logic FF;
-logic masterClear;
-
 
 modport PRIORITY(
 		input commandReg,
@@ -42,9 +38,16 @@ modport REG(
 		output  requestReg,
 		output  maskReg,
 		output  tempReg,
-		output  tempAddrReg,
-		output  tempWordReg,
-		output  statusReg	
+		output  statusReg,
+		inout   ioDataBuf,
+		inout   ioAddrBuf,
+		output  outAddrBuf
 	);
+
+modport DATAPATH(
+		inout ioDataBuf,
+		inout ioAddrBuf,
+		output outAddrBuf
+);
 
 endinterface
