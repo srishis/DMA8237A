8ec2db24f55d734be3a4d79c22bcd66acea4ecba
Rishitosh Sawant <srishis.214@gmail.com>
2019-12-03 21:42:12 -0800

    Update Dma8237aTop.sv

--- Dma8237aTop.sv
+++ Dma8237aTop.sv
@@ -1,26 +1,18 @@
 // DMA Top module
 
 `timescale 100ns/10ns
-module Dma8237aTop;
+module Dma8237aTop(dma_if dif, input logic CLK, RESET);
 
 parameter CLK_PERIOD = 200;
 parameter CLK_CYCLES = 20;
 
-logic CLK, RESET;
+// DMA modules instantiation
+DmaDatapath D1(dif, CLK, RESET);
+DmaTimingControl C1(dif, CLK, RESET);
+DmaRegisters R1(dif, CLK, RESET);
+DmaPriority P1(dif, CLK, RESET);
 
-// Interface instantiation
-dma_if dif(CLK, RESET);
-DmaControlIf cif(CLK, RESET);
-DmaDatapathIf drf(CLK, RESET);
-
-
-// DMA modules
-DmaDatapath D1(dif, cif, drf);
-DmaTimingControl C1(dif, cif, drf);
-DmaRegisters R1(dif, cif, drf);
-DmaPriority P1(dif, drf);
-
-// Clock generation 
+// 5 MHz Clock generation 
 initial begin
 	CLK = 0;
 	forever #(CLK_PERIOD) CLK = ~CLK;
