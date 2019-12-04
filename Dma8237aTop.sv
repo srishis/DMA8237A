73003e4c6052b1aec56e417422573c06c6dfce34
Rishitosh Sawant <srishis.214@gmail.com>
2019-12-04 00:15:02 -0800

    Added modports.

--- Dma8237aTop.sv
+++ Dma8237aTop.sv
@@ -1,22 +1,11 @@
 // DMA Top module
 
-`timescale 100ns/10ns
-module Dma8237aTop(dma_if dif, input logic CLK, RESET);
-
-parameter CLK_PERIOD = 200;
-parameter CLK_CYCLES = 20;
+module Dma8237aTop(dma_if.DUT dif, input logic CLK, RESET);
 
 // DMA modules instantiation
-DmaDatapath D1(dif, CLK, RESET);
-DmaTimingControl C1(dif, CLK, RESET);
-DmaRegisters R1(dif, CLK, RESET);
-DmaPriority P1(dif, CLK, RESET);
-
-// 5 MHz Clock generation 
-initial begin
-	CLK = 0;
-	forever #(CLK_PERIOD) CLK = ~CLK;
-end
-
+	DmaDatapath D1(dif.DP, CLK, RESET);
+	DmaTimingControl C1(dif.TC, CLK, RESET);
+	DmaRegisters R1(dif.REG, CLK, RESET);
+	DmaPriority P1(dif.PR, CLK, RESET);
 
 endmodule
