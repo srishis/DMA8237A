1f5001284c5fa19424a7b53c28f773feaaace966
Rishitosh Sawant <srishis.214@gmail.com>
2019-12-04 07:37:15 -0800

    Create DmaRegIf.sv

---
+++ DmaRegIf.sv
@@ -0,0 +1,33 @@
+interface DmaRegIf(input logic CLK, RESET);
+
+// DMA Registers
+ logic [5:0]  modeReg[4];
+ logic [7:0]  commandReg;
+ logic [7:0]  requestReg;
+ logic [7:0]  maskReg;
+ logic [7:0]  statusReg;
+
+
+modport DP(
+	
+ output modeReg,
+ output commandReg,
+ output requestReg,
+ output maskReg,
+ output statusReg
+
+);
+
+modport TC(
+ input modeReg,
+ input commandReg,
+ input statusReg
+);
+
+modport PR(
+ input commandReg,
+ input requestReg,
+ input maskReg,
+)
+
+endinterface
