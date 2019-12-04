43b3f855269bc7282a24b48ff17d80013a3256b4
Rishitosh Sawant <srishis.214@gmail.com>
2019-12-04 01:26:45 -0800

    Update Dma8237aTop.sv

--- Dma8237aTop.sv
+++ Dma8237aTop.sv
@@ -2,10 +2,14 @@
 
 module Dma8237aTop(dma_if.DUT dif, input logic CLK, RESET);
 
+// DMA interfaces instantiation
+DmaDatapathIf dp_if(CLK ,RESET);
+DmaControlIf cp_if(CLK ,RESET);
+	
 // DMA modules instantiation
-	DmaDatapath D1(dif.DP, CLK, RESET);
-	DmaTimingControl C1(dif.TC, CLK, RESET);
-	DmaRegisters R1(dif.REG, CLK, RESET);
-	DmaPriority P1(dif.PR, CLK, RESET);
+DmaDatapath D1(dp_if.DP, cp_if);
+DmaTimingControl C1(dif.TC, cp_if, dp_if.FSM);
+DmaRegisters R1(dif.REG, dp_if.REG, cp_if);
+DmaPriority P1(dif.PR, dp_if.PRIORITY);
 
 endmodule
