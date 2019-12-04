f31a66e39f2f10ab59e8fd258667697d54ddd7bc
Rishitosh Sawant <srishis.214@gmail.com>
2019-12-04 15:34:04 -0800

    Update Dma8237aTop.sv

--- Dma8237aTop.sv
+++ Dma8237aTop.sv
@@ -1,10 +1,9 @@
 // DMA Top module
 
-module Dma8237aTop(input logic CLK, input logic RESET);
+module Dma8237aTop(dma_if dif, input logic CLK, input logic RESET);
 
 
 // DMA interface instantiation
-dma_if dif(CLK, RESET);
 DmaControlIf cif(CLK, RESET);
 DmaRegIf rif(CLK, RESET);
 
