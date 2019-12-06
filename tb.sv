b0af99e7b6327324966ff153b7c2a02e9b97d142
Rishitosh Sawant <srishis.214@gmail.com>
2019-12-06 04:13:39 -0800

    Update tb.sv

--- tb.sv
+++ tb.sv
@@ -8,12 +8,14 @@ dma_if dif(CLK, RESET);
 Dma8237aTop DMT (dif);
 
 // CLock
-initial #10 forever  CLK = ~CLK; 
+initial begin
+forever #10  CLK = ~CLK; 
+end
 
 // Reset
 initial begin
-	RESET = 1;
-	repeat(50)@(negedge CLK) RESET = 0;
+	repeat(50)@(negedge CLK); RESET = 1;
+	repeat(100)@(negedge CLK); RESET = 0;
 end
 
 endmodule
